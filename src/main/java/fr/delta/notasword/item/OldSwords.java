26b01d2db73e1ceca33994b9f6300e38c37679b2
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-06-28 21:20:07 +0200

    update to 1.20.1

--- src/main/java/fr/delta/notasword/item/OldSwords.java
+++ src/main/java/fr/delta/notasword/item/OldSwords.java
@@ -4,12 +4,9 @@ import eu.pb4.polymer.core.api.item.PolymerItemGroupUtils;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
@@ -23,18 +20,12 @@ public class OldSwords {
     public static SwordItem DIAMOND_SWORD = create(Items.DIAMOND_SWORD, new FabricItemSettings(), "item/diamond_sword");
     public static SwordItem NETHERITE_SWORD = create(Items.NETHERITE_SWORD, new FabricItemSettings().fireproof(), "item/netherite_sword");
 
-
     private static SwordItem create(Item item, Item.Settings settings, String modelPath)
     {
         assert item instanceof SwordItem;
         var toolMaterial = ((SwordItem) item).getMaterial();
         return new OldSwordItem(toolMaterial, 3, settings, item, PolymerResourcePackUtils.requestModel(Items.SHIELD, new Identifier(MOD_ID, modelPath)).value());
     }
-
-    static final Identifier GROUP_ID = new Identifier(MOD_ID, "old_swords");
-
-    public static final RegistryKey<ItemGroup> ITEM_GROUP = RegistryKey.of(RegistryKeys.ITEM_GROUP, GROUP_ID);
-
 
     private static void registerItem(String name, Item item)
     {
@@ -50,18 +41,17 @@ public class OldSwords {
         registerItem("diamond_sword", DIAMOND_SWORD);
         registerItem("netherite_sword", NETHERITE_SWORD);
 
-        PolymerItemGroupUtils.registerPolymerItemGroup(GROUP_ID, FabricItemGroup.builder()
+        PolymerItemGroupUtils.registerPolymerItemGroup(new Identifier(MOD_ID, "old_swords"), FabricItemGroup.builder()
                 .icon(DIAMOND_SWORD::getDefaultStack)
                 .displayName(Text.translatable("category.not_a_sword.title"))
+                .entries((ctx, entries) -> {
+                    entries.add(WOODEN_SWORD);
+                    entries.add(GOLDEN_SWORD);
+                    entries.add(STONE_SWORD);
+                    entries.add(IRON_SWORD);
+                    entries.add(DIAMOND_SWORD);
+                    entries.add(NETHERITE_SWORD);
+                })
                 .build());
-
-        ItemGroupEvents.modifyEntriesEvent(ITEM_GROUP).register(content -> {
-            content.add(WOODEN_SWORD);
-            content.add(GOLDEN_SWORD);
-            content.add(STONE_SWORD);
-            content.add(IRON_SWORD);
-            content.add(DIAMOND_SWORD);
-            content.add(NETHERITE_SWORD);
-        });
     }
 }
